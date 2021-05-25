3672a047b0435aeba2a9950445e6b851b688fffe
Vlajkousa <vargirovic@yahoo.com>
2021-05-25 19:23:57 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //create new project
+        //enable version control
+        //added gitignore
+        //made initial commit by keeping track of files
+        //started new project
 
     }
 }
